66cf54a7c5256448a17837bd84071458ff053d13
Edou <tristanedou@gmail.com>
2020-01-10 12:28:24 +0100

    Let's switch to fxml

--- src/fr/isep/robotturtles/Game.java
+++ src/fr/isep/robotturtles/Game.java
@@ -2,20 +2,17 @@ package fr.isep.robotturtles;
 
 import fr.isep.robotturtles.constants.PlayerColor;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.*;
-import javafx.scene.control.Button;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Game  extends Application {
+public class Game extends Application {
     static boolean gameOn = true;
     static int playerNumber = 2;
     static Board board;
@@ -32,33 +29,12 @@ public class Game  extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
-        StackPane root = new StackPane();
-        root.setId("pane");
-        Scene scene = new Scene(root, 300, 250);
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Menu.fxml"));
+        Scene scene = new Scene(root, 600, 500);
         scene.getStylesheets().addAll(Game.class.getResource("resources/css/style.css").toExternalForm());
-
-        Button play = new Button("Start");
-        play.setLayoutX(0);
-        play.setLayoutY(0);
-        play.setOnAction(new EventHandler<ActionEvent>() {
-            public void handle(ActionEvent event) {
-                play.setText("Cliqué !");
-            }
-        });
-
-        Button exit = new Button("Quitter");
-        exit.setLayoutX(100);
-        exit.setLayoutY(100);
-        exit.setOnAction(new EventHandler<ActionEvent>() {
-            public void handle(ActionEvent event) {
-                primaryStage.close();
-                System.exit(0);
-            }
-        });
-
-        root.getChildren().add(play);
-        root.getChildren().add(exit);
+        
+        primaryStage.setOnHidden(e -> System.exit(0));
         primaryStage.setScene(scene);
         primaryStage.getIcons().add(new Image(Game.class.getResourceAsStream("resources/images/logo.png")));
         primaryStage.show();
