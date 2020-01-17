b2329cf504256f52fb50c4908890ecfaf5b40821
Edou <tristanedou@gmail.com>
2020-01-17 19:09:21 +0100

    End screen

---
+++ src/fr/isep/robotturtles/controller/EndScreenController.java
@@ -0,0 +1,36 @@
+package fr.isep.robotturtles.controller;
+
+import fr.isep.robotturtles.Main;
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+
+import java.io.IOException;
+
+public class EndScreenController {
+
+    public GridPane grid = null;
+
+    @FXML
+    public void backMenu(Event e) throws IOException {
+        Scene menu = grid.getScene();
+        Window window = menu.getWindow();
+        Stage stage = (Stage) window;
+
+        Parent root = FXMLLoader.load(Main.class.getResource("resources/scenes/Menu.fxml"));
+        Scene menuScene = new Scene(root);
+
+        stage.setScene(menuScene);
+    }
+
+    @FXML
+    public void quit(Event e){
+        System.exit(0);
+    }
+
+}
